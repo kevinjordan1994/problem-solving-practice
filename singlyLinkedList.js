aa21034b3709017361ac3a0655e6ef47b1b14c18
FingerPrintGames <kevinjordan199449@gmail.com>
2022-10-04 10:35:08 -0400

    Learn about singly linked lists

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -12,37 +12,147 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
-  push(value) {
-    const node = new Node(value);
-    if (this.length === 0) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-    this.length++;
+  push(...values) {
+    values.forEach((value) => {
+      const node = new Node(value);
+      if (this.length === 0) {
+        this.head = node;
+        this.tail = node;
+      } else {
+        this.tail.next = node;
+        this.tail = node;
+      }
+      this.length++;
+    });
     return this;
   }
 
   pop() {
-    if (this.length === 0) return;
+    if (this.length === 0) return null;
     let point = this.head;
-    let newTail;
+    let newTail = point;
     while (point !== this.tail) {
       newTail = point;
       point = point.next;
     }
-    const poppedNode = this.tail;
     this.tail = newTail;
     this.tail.next = null;
     this.length--;
-    return poppedNode;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return point;
+  }
+
+  unshift(...values) {
+    const appendableList = new SinglyLinkedList();
+    appendableList.push(...values);
+    const previousHead = this.head;
+    if (!previousHead) {
+      this.head = appendableList.head;
+      this.tail = appendableList.tail;
+    }
+    appendableList.tail.next = previousHead;
+    this.head = appendableList.head;
+    this.length += appendableList.length;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) return null;
+    const decapitatedHead = this.head;
+    this.head = decapitatedHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return decapitatedHead;
+  }
+
+  get(index) {
+    if (index >= this.length || index < 0) return null;
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  set(index, value) {
+    const targetNode = this.get(index);
+    if (!targetNode) return false;
+    targetNode.value = value;
+    return true;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.unshift(value);
+      return true;
+    }
+    const insertionPoint = this.get(index - 1);
+    if (!insertionPoint) return false;
+    if (insertionPoint === this.tail) {
+      this.push(value);
+      return true;
+    }
+    const node = new Node(value);
+    node.next = insertionPoint.next;
+    insertionPoint.next = node;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return null;
+    const previousNode = this.get(index - 1);
+    const targetNode = previousNode.next;
+    previousNode.next = targetNode.next;
+    this.length--;
+    return targetNode;
+  }
+
+  reverse() {
+    const temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let prev = null;
+    let next;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
+
+  printAllValues() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return this;
+  }
+
+  printAllNodes() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode);
+      currentNode = currentNode.next;
+    }
+    return this;
   }
 }
 
 const list = new SinglyLinkedList();
-list.push("Test");
-list.push("New Tail");
-console.log(list.push("New new tail"));
-console.log(list.pop());
+list.unshift("Value", 27, 87, "tail");
+list.set(1, "New second node");
+list.insert(2, "New third value");
+list.reverse();
